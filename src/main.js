29ff5453ed4e5fb4f27c1d97b517bc9afe05d48c
Caio Souza <ocaiomoises@gmail.com>
2025-04-05 20:07:58 -0300

    npm init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    mainWindow.loadFile("src/index.html")
+};
+
+app.whenReady().then(createWindow);
