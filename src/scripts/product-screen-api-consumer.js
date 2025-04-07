ee72844bba9ab000a9363f669902309435ee527f
Caio Souza <ocaiomoises@gmail.com>
2025-04-07 09:52:40 -0300

    Adicionado - Consumindo API
    - Consume api
    - Insere dados dinamicamente na tabela

---
+++ src/scripts/product-screen-api-consumer.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchProductsFromAPI();
+});
+
+async function fetchProductsFromAPI() {
+    try {
+        const response = await fetch('http://localhost:8080/products');
+        
+        if (!response.ok) {
+            throw new Error(`Erro HTTP: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        console.log(data);
+        
+        data.forEach(product => {
+            const row = createProductRow(product);
+            document.querySelector("#productTable tbody").appendChild(row);
+        });
+        
+    } catch (error) {
+        console.error("Erro ao buscar produtos da API:", error);
+    }
+}
+
+function createProductRow(product) {
+    const tr = document.createElement("tr");
+    tr.classList.add("product-row");
+    tr.dataset.product = JSON.stringify(product);
+
+    tr.innerHTML = `
+        <td>${product.name}</td>
+        <td>${product.volume || '0'}</td>
+        <td>${product.preco || 'R$ 0,00'}</td>
+        <td>${product.estoqueInterno || 0}</td>
+        <td>${product.category.name}</td>
+        <td class="action-buttons">
+            <button><i class="bi bi-pencil"></i></button>
+            <button><i class="bi bi-trash"></i></button>
+        </td>
+    `;
+
+    return tr;
+}
